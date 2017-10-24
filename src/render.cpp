cdf23e04bdddbb329d717e3722c98741b0c4b1b9
bmobear <toy.bubbletea@gmail.com>
2017-10-23 17:43:31 -0700

    adjusted light

--- src/render.cpp
+++ src/render.cpp
@@ -22,32 +22,56 @@ void initScene()
 	glDisable(GL_DITHER);
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_TEXTURE_2D);
-	//glEnable(GL_CULL_FACE);
-	//glCullFace(GL_BACK);
 	glShadeModel(GL_SMOOTH);
 
-	//glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_FALSE);
-	//glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_FALSE);
-	
+	glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
+	glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
 
 	// material
-	GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-	GLfloat mat_shininess[] = { 50.0 };
+	GLfloat mat_ambient_and_diffuse[] = {1.0, 1.0, 1.0, 1.0};
+	GLfloat mat_specular[] = {0.5, 0.5, 0.5, 1.0};
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, mat_ambient_and_diffuse);
 	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+	glMaterialf(GL_FRONT, GL_SHININESS, 50.0);
 
-	// light 0
-	GLfloat light0_position[] = { 0.2, 0.5, 0.8, 0.0 };
-	glLightfv(GL_LIGHT0, GL_POSITION, light0_position);
+	// light 0: default
+	// ambient 0,0,0,1
+	// diffuse 1,1,1,1
+	// specular 1,1,1,1
+	// position 0,0,1,0
+	GLfloat l0_ambient[] = {0.0, 0.0, 0.0, 1.0};
+	GLfloat l0_diffuse[] = {0.5, 0.5, 0.5, 1.0};
+	GLfloat l0_specular[] = {0.0, 0.0, 0.0, 0.0};
+	GLfloat l0_position[] = {0.25, 0.85, 0.75, 0.0};
+	glLightfv(GL_LIGHT0, GL_AMBIENT, l0_ambient);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, l0_diffuse);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, l0_specular);
+	glLightfv(GL_LIGHT0, GL_POSITION, l0_position);
 
 	// light 1
-	GLfloat light1_position[] = { 0.0, 1.0, 0.0, 0.0 };
-	glLightfv(GL_LIGHT1, GL_POSITION, light1_position);
+	GLfloat l1_ambient[] = {0.0, 0.0, 0.0, 1.0};
+	GLfloat l1_diffuse[] = {0.1, 0.0, 0.0, 1.0};
+	GLfloat l1_specular[] = {0.0, 0.0, 0.0, 0.0};
+	GLfloat l1_position[] = {-0.7, -0.5, 0.0, 0.0};
+	glLightfv(GL_LIGHT1, GL_AMBIENT, l1_ambient);
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, l1_diffuse);
+	glLightfv(GL_LIGHT1, GL_SPECULAR, l1_specular);
+	glLightfv(GL_LIGHT1, GL_POSITION, l1_position);
 
+	// light 2
+	GLfloat l2_ambient[] = {0.0, 0.0, 0.0, 1.0};
+	GLfloat l2_diffuse[] = {0.0, 0.0, 0.1, 1.0};
+	GLfloat l2_specular[] = {0.1, 0.1, 0.1, 1.0};
+	GLfloat l2_position[] = {0.7, -0.5, 0.0, 0.0};
+	glLightfv(GL_LIGHT2, GL_AMBIENT, l2_ambient);
+	glLightfv(GL_LIGHT2, GL_DIFFUSE, l2_diffuse);
+	glLightfv(GL_LIGHT2, GL_SPECULAR, l2_specular);
+	glLightfv(GL_LIGHT2, GL_POSITION, l2_position);
 
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
-//	glEnable(GL_LIGHT1);
+	glEnable(GL_LIGHT1);
+	glEnable(GL_LIGHT2);
 
 	// set view
 	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
